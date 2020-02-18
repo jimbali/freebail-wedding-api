95866bececad94bb903df2dd2c20aeba567c0cb1
Jim Bailey <jbailey@ukcloud.com>
2020-02-18 21:46:53 +0000

    Allow all origins. Fix Docker Compose.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,20 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  after_action :set_cors_headers
+
   # Include default headers by default
   def self.make_response!(request)
     ActionDispatch::Response.create.tap do |res|
       res.request = request
     end
+  end
+
+  def set_cors_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] =
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 end
