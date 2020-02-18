348da0f0bddfa2eb56cd0b396abaa7940a72a62d
Jim Bailey <jbailey@ukcloud.com>
2020-02-18 16:58:34 +0000

    Allow requests from all origins in production mode

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,4 +84,11 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins '*'
+      resource '*', headers: :any, methods: %i[get options patch post]
+    end
+  end
 end
